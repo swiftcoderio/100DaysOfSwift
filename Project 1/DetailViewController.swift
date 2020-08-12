d2d3fd3e6a2d71ee856f63997247d86509fecd6a
Irfan Hussain <swiftcoderio@gmail.com>
2020-08-11 23:26:31 -0400

    Project 1 from 100 Days of Swift

---
+++ Project 1/DetailViewController.swift
@@ -0,0 +1,52 @@
+//
+//  DetailViewController.swift
+//  Project 1
+//
+//  Created by Swift Coder on 3/28/20.
+//  Copyright © 2020 Swift Coder. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+    var selectedImage: String?
+    var xImage: Int?
+    var yImage: Int?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Picture \(xImage!) of \(yImage!)"
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
